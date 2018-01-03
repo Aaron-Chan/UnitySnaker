f1f1e6120ef573b7b76adfa1442a8b6b014233e2
chenxiaocheng <1903682265@qq.com>
2018-01-03 23:23:09 +0800

    加入主城

---
+++ Assets/Snaker/Module/Login/LoginModule.cs
@@ -0,0 +1,40 @@
+﻿using SGF.Module.Framework;
+using SGF.UI.Framework;
+using Snaker.Service.User;
+using Snaker.Service.UserManager.Data;
+
+namespace Snaker.Module
+{
+	public class LoginModule : BusinessModule
+	{
+		protected override void Show(object arg)
+		{
+			UIManager.Instance.OpenPage(UIDef.UILoginPage);
+		}
+
+
+		public void Login(uint id, string name, string pwd)
+		{
+			UserData ud = new UserData();
+			ud.id = (uint) id;
+			ud.name = name;
+			ud.defaultSnakeId = 1;
+			OnLoginSuccess(ud);
+		}
+
+		private void OnLoginSuccess(UserData ud)
+		{
+			//更新用户信息
+			UserManager.Instance.UpdateMainUserData(ud);
+
+			//保存
+			AppConfig.Value.mainUserData = ud;
+			AppConfig.Save();
+
+			GlobalEvent.onLogin.Invoke(true);
+
+			//进入主页面
+			UIManager.Instance.EnterMainPage();
+		}
+	}
+}
---
+++ Assets/Snaker/UI/Login/UILoginPage.cs
@@ -0,0 +1,46 @@
+﻿using SGF;
+using SGF.Module.Framework;
+using SGF.UI.Framework;
+using Snaker.Module;
+using Snaker.Service.UserManager.Data;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Snaker.UI.Login
+{
+	public class UILoginPage:UIPage
+	{
+		public InputField inputId;
+		public InputField inputName;
+
+
+		protected override void OnOpen(object arg)
+		{
+			base.OnOpen(arg);
+			UserData ud = AppConfig.Value.mainUserData;
+			inputName.text = ud.name;
+			inputId.text = ud.id.ToString();
+
+		}
+
+
+
+		public void OnBtnLogin()
+		{
+			uint userId = 0;
+			this.Log(" null"+(inputId == null));
+			uint.TryParse(inputId.text, out userId);
+			string userName = inputName.text.Trim();
+			if (userId == 0)
+			{
+				userId = (uint)Random.Range(100000, 999999);
+			}
+
+			var module = ModuleManager.Instance.GetModule(ModuleDef.LoginModule) as LoginModule;
+			if (module != null)
+			{
+				module.Login(userId, userName, "");
+			}
+		}
+	}
+}
